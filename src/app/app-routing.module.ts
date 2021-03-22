a8b40633753a96e0ca7d82a2dc97f347fed99c05
benjamin-bravo <benjamin.bravo@usach.cl>
2021-03-22 18:20:28 -0300

    day end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive-routing.module').then(m => m.ReactiveRoutingModule)
+  },
+  {
+    path: 'template',
+    loadChildren: () => import('./template/template-routing.module').then(m => m.TemplateRoutingModule)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
